462d2b1220b0849de873ce35df97bc67c52810f6
Snehal Kadav <snehal.kadav1411@gmail.com>
2021-12-02 22:55:11 +0530

    deploying it again

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
+    const { statusCode } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
